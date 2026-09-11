821ef523c179fe8267840fd2935d6943cffa4c6a
Rocky-2247 <121039153+Rocky-2247@users.noreply.github.com>
2026-09-11 11:05:40 +0530

    second_commit

--- Ecommerce/backend/routes/webhookRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { handlePaymentWebhook } from '../controllers/webhookController.js';
-
-const router = express.Router();
-
-router.post('/payment', handlePaymentWebhook);
-
-export default router;
